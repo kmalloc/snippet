2228b8fc3bbda0a853f470e3d284fc1510fab693
kmalloc <kmalloc@live.com>
2014-11-19 21:41:38 +0800

    memory model

---
+++ memory/out_of_order.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+// #include <thread>
+#include <pthread.h>
+
+using namespace std;
+
+volatile int g_x = 0, g_y = 0;
+volatile int g_a = 0, g_b = 0;
+
+void* foo1(void*)
+{
+    g_x = 42;
+    g_a = g_y;
+
+    return NULL;
+}
+
+void* foo2(void*)
+{
+    g_y = 24;
+    g_b = g_x;
+    return NULL;
+}
+
+int main()
+{
+    for (int i = 0; i < 100000; ++i)
+    {
+        pthread_t t1, t2;
+
+        g_x = g_y = g_a = g_b = 0;
+        pthread_create(&t1, NULL, &foo1, NULL);
+        pthread_create(&t2, NULL, &foo2, NULL);
+
+        pthread_join(t1, NULL);
+        pthread_join(t2, NULL);
+
+        if (g_a == 0 && g_b == 0) cout << "g_a == g_b == 0" << endl;
+    }
+
+    return 0;
+}
+
